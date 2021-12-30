92a28ba3f3b5e0bb3d7951c1eefe7853ec86ce38
Josh Kewley <josh.kewley@slalom.com>
2021-12-30 12:53:14 -0600

    Custom archtypes for landing zones

--- level0/main.tf
+++ level0/main.tf
@@ -19,9 +19,39 @@ module "enterprise_scale" {
   root_parent_id = data.azurerm_client_config.core.tenant_id
   root_id        = var.root_id
   root_name      = var.root_name
-  # library_path = "${path.root}/lib"
+  library_path   = "${path.root}/lib"
 
   # default_location = "eastus2"
 
   deploy_demo_landing_zones = true
+
+  custom_landing_zones = {
+    "${var.root_id}-online-example-1" = {
+      display_name               = "${upper(var.root_id)} Online Example 1"
+      parent_management_group_id = "${var.root_id}-landing-zones"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "customer_online"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+    "${var.root_id}-online-example-2" = {
+      display_name               = "${upper(var.root_id)} Online Example 2"
+      parent_management_group_id = "${var.root_id}-landing-zones"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id = "customer_online"
+        parameters = {
+          Deny-Resource-Locations = {
+            listOfAllowedLocations = ["eastus", ]
+          }
+          Deny-RSG-Locations = {
+            listOfAllowedLocations = ["eastus", ]
+          }
+        }
+        access_control = {}
+      }
+    }
+  }
 }
