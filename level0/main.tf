4ed2520b7879965504e2b5b84410cef3e748f225
Josh Kewley <josh.kewley@slalom.com>
2022-01-05 11:16:51 -0600

    Refactor providers for multiple subscriptions

--- level0/main.tf
+++ level0/main.tf
@@ -1,19 +1,33 @@
-# You can use the azurerm_client_config data resource to dynamically
-# extract connection settings from the provider configuration.
+# Obtain client configuration from the un-aliased provider
+data "azurerm_client_config" "core" {
+  provider = azurerm
+}
 
-data "azurerm_client_config" "core" {}
+# Obtain client configuration from the "management" provider
+data "azurerm_client_config" "management" {
+  provider = azurerm.management
+}
+
+# Obtain client configuration from the "connectivity" provider
+data "azurerm_client_config" "connectivity" {
+  provider = azurerm.connectivity
+}
+
+# Obtain client configuration from the "identity" provider
+data "azurerm_client_config" "identity" {
+  provider = azurerm.identity
+}
 
 # Call the caf-enterprise-scale module directly from the Terraform Registry
 # pinning to the latest version
-
 module "enterprise_scale" {
   source  = "Azure/caf-enterprise-scale/azurerm"
   version = "1.1.1" # latest as of 12/29/21
 
   providers = {
     azurerm              = azurerm
-    azurerm.connectivity = azurerm
-    azurerm.management   = azurerm
+    azurerm.management   = azurerm.management
+    azurerm.connectivity = azurerm.connectivity
   }
 
   root_parent_id = data.azurerm_client_config.core.tenant_id
@@ -21,12 +35,17 @@ module "enterprise_scale" {
   root_name      = var.root_name
   library_path   = "${path.root}/lib"
 
-  # default_location = "eastus2"
-
-  deploy_demo_landing_zones = true
-
   deploy_connectivity_resources = true
-  subscription_id_connectivity  = "2d479f58-3999-4c7d-ba51-737f6b8032b1"
+  subscription_id_connectivity  = data.azurerm_client_config.connectivity.subscription_id
+
+  deploy_management_resources = true
+  subscription_id_management  = data.azurerm_client_config.management.subscription_id
+
+  deploy_identity_resources = true
+  subscription_id_identity  = data.azurerm_client_config.identity.subscription_id
+
+  # default_location = "eastus2"
+  deploy_demo_landing_zones = true
   custom_landing_zones = {
     "${var.root_id}-online-example-1" = {
       display_name               = "${upper(var.root_id)} Online Example 1"
--- level0/terraform.tf
+++ level0/terraform.tf
@@ -6,10 +6,46 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 2.77.0" # version is 2.90.0 on 12/29/21
+      configuration_aliases = [
+        azurerm.connectivity,
+        azurerm.management,
+      ]
     }
   }
 }
 
 provider "azurerm" {
+  features {}
+}
+
+# Declare an aliased provider block using your preferred configuration.
+# This will be used for the deployment of all "Connectivity resources" to the specified `subscription_id`.
+provider "azurerm" {
+  alias           = "connectivity"
+  subscription_id = "2d479f58-3999-4c7d-ba51-737f6b8032b1"
+  features {}
+}
+
+# Declare a standard provider block using your preferred configuration.
+# This will be used for the deployment of all "Management resources" to the specified `subscription_id`.
+provider "azurerm" {
+  alias           = "management"
+  subscription_id = "32236649-0cf4-4ee5-852f-3b3d865667fc"
+  features {}
+}
+
+# Declare a standard provider block using your preferred configuration.
+# This will be used for the deployment of all "Identity resources" to the specified `subscription_id`.
+provider "azurerm" {
+  alias           = "identity"
+  subscription_id = "47fb6973-1b23-4388-8078-4d72c71a3247"
+  features {}
+}
+
+# Declare an aliased provider block using your preferred configuration.
+# This will be used for the deployment of landing zone to the specified `subscription_id`.
+provider "azurerm" {
+  alias           = "lz"
+  subscription_id = "cdc34bfa-1b00-46f4-9868-631d7a957de1"
   features {}
 }
